93a276621a3b2cc18fb70998d3ab5ac36d71d1b9
tintin <ljttl3q04t@gmail.com>
2023-05-30 20:43:31 +0700

    book update

--- services/urls.py
+++ services/urls.py
@@ -5,6 +5,7 @@ from .views import change_borrowing_status, create_borrowing
 
 urlpatterns = [
     path('list/', views.viewListBook, name='view_list'),
+    path('book/<int:book_id>/update/', views.BookUpdateAPIView.as_view(), name='book-update'),
 
     path('api/borrowing/<int:borrowing_id>/status/', change_borrowing_status, name='change_borrowing_status'),
     path('api/borrowing/create/', create_borrowing, name='create_borrowing'),
--- services/views.py
+++ services/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework.pagination import PageNumberPagination
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Book, User
 from .serializers import BookSerializer
 from django.http import JsonResponse
@@ -19,6 +21,21 @@ class BookListAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     pagination_class = CustomPagination
+
+
+class BookUpdateAPIView(APIView):
+    def post(self, request, book_id):
+        try:
+            book = Book.objects.get(pk=book_id)
+        except Book.DoesNotExist:
+            return Response({"error": "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 def index(request):
