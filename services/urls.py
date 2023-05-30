376ddf7d4b06afb06c03b5a0ba49503e35431a74
Tony Pham <dicolo17@gmail.com>
2023-05-30 13:55:05 +0700

    book-list api

--- services/urls.py
+++ services/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('list/', views.viewListBook, name='view_list'),
+    path('book/list/', views.BookListAPIView.as_view(), name='book-list'),
     path("", views.index, name="index"),
 ]
--- services/views.py
+++ services/views.py
@@ -1,12 +1,22 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import generics
 from .models import Book
-from rest_framework import views
+from .serializers import BookSerializer
 
+class CustomPagination(PageNumberPagination):
+    page_size = 10  # Set the desired page size
+    page_size_query_param = 'page_size'  # Customize the query parameter for specifying the page size
+    max_page_size = 100  # Set the maximum allowed page size
+
+
+class BookListAPIView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    pagination_class = CustomPagination
 
 def index(request):
     return render(request, "services/index.html")
-
 
 def viewListBook(request):
     list_book = Book.objects.all()
