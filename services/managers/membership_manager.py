305a46e6174f39dcbf6eb48f58411fdcd556fc3e
dzung <ljttl3q04t@gmail.com>
2023-07-04 15:35:41 +0700

    remove cache

--- services/managers/membership_manager.py
+++ services/managers/membership_manager.py
@@ -5,13 +5,13 @@ from services.serializers import BookClubSerializer
 def get_user_club(user: User):
     return Membership.objects.filter(member__user=user).values_list('book_club_id', flat=True)
 
-@simple_cache_data(**CACHE_CLUB_GET_INFOS_DICT)
+# @simple_cache_data(**CACHE_CLUB_GET_INFOS_DICT)
 def get_clubs():
     result = BookClub.objects.all()
     serializer = BookClubSerializer(instance=result, many=True)
     return {item['id']: item for item in serializer.data}
 
-@simple_cache_data(**CACHE_KEY_MEMBER_INFOS)
+# @simple_cache_data(**CACHE_KEY_MEMBER_INFOS)
 def get_member_infos():
     results = Member.objects.all()
     data = {}
