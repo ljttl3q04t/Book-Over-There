a15894c5051612e8c529893fa26fe66147a978b8
dzung <ljttl3q04t@gmail.com>
2023-07-21 23:27:36 +0700

    fix club_book/add

--- d_free_book/views.py
+++ d_free_book/views.py
@@ -57,9 +57,11 @@ class ClubBookAddView(APIView):
         serializer = ClubBookAddSerializer(data=request.data)
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
         club_ids = membership_manager.get_membership_records(request.user, is_staff=True).flat_list('book_club_id')
         if serializer.data.get('club_id') not in club_ids:
             return Response({'error': 'Permission denied'}, status=status.HTTP_400_BAD_REQUEST)
+
         book = get_book_records(book_name=serializer.data.get('name')).first()
 
         if manager.get_club_book_records(code=serializer.data.get('code'),
